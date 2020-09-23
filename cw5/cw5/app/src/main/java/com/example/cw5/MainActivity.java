af638bc3b8037a680fe86123b08b580a1925d63b
aishaALKM <70426536+aishaALKM@users.noreply.github.com>
2020-09-23 20:28:20 +0300

    done

--- cw5/cw5/app/src/main/java/com/example/cw5/MainActivity.java
+++ cw5/cw5/app/src/main/java/com/example/cw5/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.cw5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button a=findViewById(R.id.nextPage);
+        EditText b=findViewById(R.id.name);
+        EditText c=findViewById(R.id.age);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent o = new Intent( MainActivity.this,MainActivity2.class);
+
+            }
+        });
+
+
+
+
     }
 }
---
+++ cw5/cw5/app/src/main/java/com/example/cw5/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.cw5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView name=findViewById(R.id.)
+
+        Bundle aisha = getIntent().getExtras();
+        aisha, ("info");
+    }
+}
